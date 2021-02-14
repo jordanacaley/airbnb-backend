49a8c146c2810df7c3fed93b71201e6e6ea4112d
Jordan <jordancaley10@gmail.com>
2021-02-14 17:34:27 +0100

    Add custom validation to make sure reservation doesn't overlap with others for same listing

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,17 +2,34 @@ class Reservation < ApplicationRecord
   belongs_to :guest, class_name: "User"
   belongs_to :listing
 
+  # Confirm start date is before end date
   validate :start_must_be_before_end_date
 
-  # Confirm start date is before end date
   def start_must_be_before_end_date
     return unless start_date and end_date
-    errors.add(:start_date, "must be before end time") unless start_date < end_date
+    errors.add(:start_date, "must be before end date") unless start_date < end_date
+  end
+
+  # Confirm booking doesn't overlap with any others
+  validate :validate_other_booking_overlap
+
+  def period
+    start_date..end_date
+  end
+
+  private
+
+  def validate_other_booking_overlap
+    other_bookings = Reservation.all
+    is_overlapping = other_bookings.any? do |other_booking|
+      period.overlaps?(other_booking.period)
+    end
+    errors.add(:overlaps_with_other) if is_overlapping
   end
 
   # Get duration of reservation
   def duration
     (self.end_date.to_i/60/60/24) - (self.start_date.to_i/60/60/24)
   end
-  
+
 end
